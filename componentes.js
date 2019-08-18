0f0b63862bbd7fc24a48a377f5e87df941c8e618
Carlos Wilfredo Romero Maradiaga <romerocarlosmaradiaga@unitec.edu>
2019-08-18 17:56:03 -0600

    Galeria

--- componentes.js
+++ componentes.js
@@ -1,18 +1,51 @@
-Vue.component('navBar', {
+Vue.component('generalbar', {
     data(){
-        return {
-            name: 'Will'
-        }
+        return {nombre:"Sing In",}
     },
-    template: `
-    <div class="nav-wrapper grey darken-2" >
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li><a><i class="material-icons">menu</i></a></li>
+    template: 
+    `
+    <nav>
+        <!--Menu-->
+        <ul id="opcionesInicio" class="dropdown-content">
+            <li><a href="galery.html" class="grey darken-2 white-text">Galería Online</a></li>
+            <li class="divider" ></li>
+            <li><a href="buy.html" class="grey darken-2 white-text">Alquilar o Comprar Obras</a></li>
+            <li class="divider" ></li>
+            <li><a href="opinions.html" class="grey darken-2 white-text">Sugerencias</a></li>
         </ul>
-        <a href="index.html" class="brand-logo center"><img class="responsive-img" src="Logo.png" style="width:60px"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="collapsible.html">Sign In</a></li>
-        </ul>
-    </div>
+        <div id="NavBar" class="nav-wrapper grey darken-2" >
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a class="dropdown-trigger" href="#!" data-target="opcionesInicio"><i class="material-icons">menu</i></a></li>
+            </ul>
+            <a href="index.html" class="brand-logo center"><img class="responsive-img" src="images/Logo.png" style="width:60px"></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#!"><i class="material-icons left">account_box</i>{{ nombre }}</a></li>
+            </ul>
+        </div>
+    </nav> 
+    `
+});
+
+Vue.component('generalfooter', {
+    data(){
+        return {nombre:"Sing In",}
+    },
+    template: 
+    `
+    <footer class="page-footer grey darken-2">
+        <div class="container">
+            <div class="row">
+                <div class="col s3"><a class="grey-text text-lighten-3" href="galery.html"><i class="material-icons left">color_lens</i>Galería Online</a></div>
+                <div class="col s3"><a class="grey-text text-lighten-3" href="buy.html"><i class="material-icons left" >place</i>Alquilar o Comprar</a></div>
+                <div class="col s3"><a class="grey-text text-lighten-3" href="index.html"><i class="material-icons left">help</i>Sugerencias</a></div>
+                <div class="col s3"><a class="grey-text text-lighten-3" href="#!"><i class="material-icons left">account_box</i>{{ nombre }}</a></div>
+            </div>            
+          </div>
+          <div class="footer-copyright">
+            <div class="container ">
+                © 2019 - Carlos Wilfredo Romero Maradiaga
+            </div>
+          </div>
+    </footer>
     `
 });
