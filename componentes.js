476bd063e32d70b09d552bc3f1734c57b21cf407
Carlos Wilfredo Romero Maradiaga <romerocarlosmaradiaga@unitec.edu>
2019-08-19 07:07:12 -0600

    Terminado De verdad

--- componentes.js
+++ componentes.js
@@ -13,7 +13,7 @@ Vue.component('generalbar', {
             <li class="divider" ></li>
             <li><a href="buy.html" class="grey darken-2 white-text">Alquilar o Comprar Obras</a></li>
             <li class="divider" ></li>
-            <li><a href="opinions.html" class="grey darken-2 white-text">Sugerencias</a></li>
+            <!--<li><a href="opinions.html" class="grey darken-2 white-text">Sugerencias</a></li>-->
         </ul>
         <div id="NavBar" class="nav-wrapper grey darken-2" >
             <ul id="nav-mobile" class="left hide-on-med-and-down">
@@ -21,7 +21,7 @@ Vue.component('generalbar', {
             </ul>
             <a href="index.html" class="brand-logo center"><img class="responsive-img" src="images/Logo.png" style="width:60px"></a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#!"><i class="material-icons left">account_box</i>{{ nombre }}</a></li>
+                <li><a href="perfil.html"><i class="material-icons left">account_box</i>{{ nombre }}</a></li>
             </ul>
         </div>
     </nav> 
@@ -40,7 +40,7 @@ Vue.component('generalfooter', {
                 <div class="col s3"><a class="blue-text text-lighten-2 " href="index.html"><i class="material-icons left">home</i>Inicio</a></div>
                 <div class="col s3"><a class="grey-text text-lighten-3" href="galery.html"><i class="material-icons left">color_lens</i>Galería Online</a></div>
                 <div class="col s3"><a class="grey-text text-lighten-3" href="buy.html"><i class="material-icons left" >place</i>Alquilar o Comprar</a></div>
-                <div class="col s3"><a class="grey-text text-lighten-3" href="index.html"><i class="material-icons left">help</i>Sugerencias</a></div>
+                <div class="col s3"><a class="grey-text text-lighten-3" href="perfil.html"><i class="material-icons left">account_box</i>Perfil</a></div>
             </div>            
           </div>
           <div class="footer-copyright">
